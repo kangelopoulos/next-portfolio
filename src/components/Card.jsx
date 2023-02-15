ba2db5082b89cfcba3acfbbecdb198ae09f63e90
Katharine Angelopoulos <katharine.angelopoulos@gmail.com>
2023-02-15 17:54:05 -0600

    add card boilerplate + dropdown

---
+++ src/components/Card.jsx
@@ -0,0 +1,13 @@
+export default function Card() {
+  return (
+    <div className="card">
+      <h2>Title</h2>
+      <img alt="fake"/>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris consequat risus nisi. Mauris eleifend elementum enim, eget aliquet neque accumsan posuere. </p>
+      <div class="tech-badges">
+        
+      </div>
+    </div>
+
+  )
+}
