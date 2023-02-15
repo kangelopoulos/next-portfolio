c439e530c5b1cbe43f11f190e85484d75e2019d4
Katharine Angelopoulos <katharine.angelopoulos@gmail.com>
2023-02-15 17:56:15 -0600

    add boilerplate to card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -5,9 +5,10 @@ export default function Card() {
       <img alt="fake"/>
       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris consequat risus nisi. Mauris eleifend elementum enim, eget aliquet neque accumsan posuere. </p>
       <div class="tech-badges">
-        
+        {/* put tech badges here for each tech */}
       </div>
+      <a>Github</a>
+      <a href="">Site</a>
     </div>
-
   )
 }
