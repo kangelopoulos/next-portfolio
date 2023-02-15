4e6a231dbf05d148b48ad010f9a27825efdd3c37
Katharine Angelopoulos <katharine.angelopoulos@gmail.com>
2023-02-15 15:17:19 -0600

    add basic routing

---
+++ src/app/home/page.jsx
@@ -0,0 +1,14 @@
+const Home: FC = () => {
+  return (
+    <div className="center">
+      <div>
+        <p>Welcome!</p>
+        <><p className="my-name">I'm </p><h2 className="my-name">Kate Angelopoulos.</h2></>
+        <p>I love taking projects from ideation to production.</p>
+        <p>Currently building full-stack applications in JavaScript and TypeScript using React, Redux, Node, Express, MongoDB, and Postgres. Cloud computing enthusiast studying for certification - check out my recent tech talk on AWS/Cloudtech!</p>
+        <p>Author and contributor to Better Programming on Medium. Proud co-creator and maintainer of Denosoar, an open-source memory tracking and load testing tool built in Deno with a Fresh/Preact GUI utilizing island architecture. See my Github for the latest updates.</p>
+        <p>P.S. Click the lightbulb!</p>
+      </div>
+    </div>
+  )
+}
