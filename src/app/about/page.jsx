16cade651f5a6a624e78e249c24d11cf6a83265f
Katharine Angelopoulos <katharine.angelopoulos@gmail.com>
2023-02-15 14:47:02 -0600

    add about structure

---
+++ src/app/about/page.jsx
@@ -0,0 +1,14 @@
+export default function About() {
+  return (
+    <div id="aboutme">
+      <div className="side">
+        <p>Me &</p><img src="https://kate-portfolio-bucket.s3.us-east-2.amazonaws.com/me%26odin.png" alt="me and my dog" /><p>Odin</p>
+      </div>
+      <div className="mid">
+        <p>I fell in love with software engineering seven years ago when I picked up data analysis in Python. A few years later, I discovered full-stack web development and it has been my passion ever since.</p>
+        <p>Aside from web development, I&rsquo;m also a long distance runner and biker. I love camping, hiking, backpacking, and pretty much anything that involves the outdoors. Nothing quite beats a quiet night in a tent under the stars.</p>
+        <p>In my off hours, you&rsquo;d find me road tripping to national parks with my bass fishing rod and my big shepherd mix, Odin, probably with a book about some exciting technology tucked under my arm.</p>
+      </div>
+    </div>
+  )
+}
