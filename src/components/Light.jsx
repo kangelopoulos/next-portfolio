552ec3a792a518484dc6df07fb00ecbc45472f44
Katharine Angelopoulos <katharine.angelopoulos@gmail.com>
2023-02-15 18:12:49 -0600

    add lightbulb component code

--- src/components/Light.jsx
+++ src/components/Light.jsx
@@ -1,3 +1,24 @@
 export default function Light() {
-  
+  return (
+    <div className="spacing">
+      <div className="wrapper" onClick={lights}>
+        <div className="lightbulb">
+          <div className="base">
+            <div className="bulb">
+              <div className="inner">
+                <div className="ring"></div>
+                <div className="ring"></div>
+              </div>
+            </div>
+            <div className="content">
+              <div className="next"></div>
+              <div className="next"></div>
+              <div className="next"></div>
+            </div>
+            <div className="footer"></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 }
