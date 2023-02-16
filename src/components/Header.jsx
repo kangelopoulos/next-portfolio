11793997f3bac35b48ef5d56ac68a893b979718d
Katharine Angelopoulos <katharine.angelopoulos@gmail.com>
2023-02-15 18:16:34 -0600

    add light to header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,18 @@
 import Link from "next/link";
+import Light from "./Light";
 
 export default function Header() {
   return (
-    
-    <div className="col">
-      <a className="social-link" rel="noreferrer" target="_blank" href="https://www.linkedin.com/in/kangelopoulos/">LinkedIn</a>
-      <a className="social-link" rel="noreferrer" target="_blank" href="https://github.com/kangelopoulos">Github</a>
-      <a className="social-link" rel="noreferrer" target="_blank" href="https://medium.com/@kangelopoulos">Medium</a>
-      <a className="social-link" rel="noreferrer" target="_blank" href="https://denosoar.deno.dev/">Denosoar</a>
-      <button>Resume</button>      
-      <button>Contact</button>
+    <div id="navbar">
+      <Light />
+      <div className="row">
+        <a className="social-link" rel="noreferrer" target="_blank" href="https://www.linkedin.com/in/kangelopoulos/">LinkedIn</a>
+        <a className="social-link" rel="noreferrer" target="_blank" href="https://github.com/kangelopoulos">Github</a>
+        <a className="social-link" rel="noreferrer" target="_blank" href="https://medium.com/@kangelopoulos">Medium</a>
+        <a className="social-link" rel="noreferrer" target="_blank" href="https://denosoar.deno.dev/">Denosoar</a>
+        <button>Resume</button>      
+        <button>Contact</button>
+      </div>
     </div>
   )
 }
