428ffdc30d980bf45ecd8b25b529ded91c00fd23
Katharine Angelopoulos <katharine.angelopoulos@gmail.com>
2023-02-15 18:12:11 -0600

    add light bulb component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,13 +2,14 @@ import Link from "next/link";
 
 export default function Header() {
   return (
-  <div className="col">
-    <a className="social-link" rel="noreferrer" target="_blank" href="https://www.linkedin.com/in/kangelopoulos/">LinkedIn</a>
-    <a className="social-link" rel="noreferrer" target="_blank" href="https://github.com/kangelopoulos">Github</a>
-    <a className="social-link" rel="noreferrer" target="_blank" href="https://medium.com/@kangelopoulos">Medium</a>
-    <a className="social-link" rel="noreferrer" target="_blank" href="https://denosoar.deno.dev/">Denosoar</a>
-    <button>Resume</button>      
-    <button>Contact</button>
-  </div>
+    
+    <div className="col">
+      <a className="social-link" rel="noreferrer" target="_blank" href="https://www.linkedin.com/in/kangelopoulos/">LinkedIn</a>
+      <a className="social-link" rel="noreferrer" target="_blank" href="https://github.com/kangelopoulos">Github</a>
+      <a className="social-link" rel="noreferrer" target="_blank" href="https://medium.com/@kangelopoulos">Medium</a>
+      <a className="social-link" rel="noreferrer" target="_blank" href="https://denosoar.deno.dev/">Denosoar</a>
+      <button>Resume</button>      
+      <button>Contact</button>
+    </div>
   )
 }
