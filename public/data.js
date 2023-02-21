4f024c2f97ae9f29c04f36a14d4da04b15199f35
Katharine Angelopoulos <katharine.angelopoulos@gmail.com>
2023-02-20 18:19:34 -0600

    add data

---
+++ public/data.js
@@ -0,0 +1,31 @@
+export const techs = [
+  "Firebase", "Node", "Express", "Redux", "RTK", "PostgreSQL", "bcrypt", "Git", "Github", "JavaScript", "Webpack", "SCSS", "React", "JWT", "Deno", "Fresh", "Oak", "TypeScript", "CI/CD", "Preact", "Tailwind CSS", "CSS", "Chart.js", "WebSockets", "Docker", "AWS"
+]
+
+
+export const fakeData = [
+  {
+    name: "Cafe Kvasir", 
+    imgUrl: "https://kate-portfolio-bucket.s3.us-east-2.amazonaws.com/kvasir-coffee.png",
+    description: "A tool to connect language learning partners with live, peer-to-peer chat via Firebase.",
+    technologies: ["Firebase", "Node", "Express", "Redux", "RTK", "PostgreSQL", "bcrypt", "Git", "Github", "JavaScript", "Webpack", "SCSS", "React", "JWT"],
+    githubUrl: "https://github.com/Dragonite-Kvasir/Kvasir", 
+    deploymentUrl: ""
+  },
+  {
+    name: "Nurture", 
+    imgUrl: "https://kate-portfolio-bucket.s3.us-east-2.amazonaws.com/nurture.png",
+    description: "Don't overload your students with homework - use Nurture.",
+    technologies: ["Express", "Node", "Redux", "RTK", "PostgreSQL", "bcrypt", "Git", "Github", "JavaScript", "Webpack", "SCSS", "React", "JWT"],
+    githubUrl: "https://github.com/PGCHedka/NurtureSaaS", 
+    deploymentUrl: ""
+  },
+  {
+    name: "Denosoar", 
+    imgUrl: "https://kate-portfolio-bucket.s3.us-east-2.amazonaws.com/super-deno.png",
+    description: "An open source CLI tool that allows Deno developer's to identify and eliminate memory leaks.",
+    technologies: ["Deno", "Fresh", "Oak", "TypeScript", "CI/CD", "Preact", "Tailwind CSS", "Git", "Github", "CSS", "Chart.js", "WebSockets"],
+    githubUrl: "https://github.com/oslabs-beta/denosoar", 
+    deploymentUrl: "https://deno.land/x/denosoar@v1.0.0"
+  }
+]
